c52e47c6e5c8187d03127d5b3cbff7e2e0ee5500
itasworkshop <itasworkshop@gmail.com>
2024-03-12 14:48:59 +0530

    unittest commit

---
+++ examples/seleniumConcepts/testGoogleSearch.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
+
+print("Getting you chrome browser!!!!")
+driver  = webdriver.Chrome()
+
+
+driver.maximize_window()
+#driver.implicitly_wait(3)
+#time.sleep(3)
+driver.get("https://www.google.com/")
+#search_box = driver.find_element("name","q")
+#search_box = driver.find_element("id","APjFqb")
+#search_box = driver.find_element(By.ID,"APjFqb")
+search_box = driver.find_element(By.XPATH,'//*[@id="APjFqb"]')
+search_box.send_keys("hello")
+search_box.submit()
+time.sleep(3)
+driver.close()
+print("Closing your chrome browser!!!!")
